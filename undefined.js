f1159da3f300ecfeff2028c6aaef1769a56a4566
sakibhossain <sakibhossainceo@gmail.com>
2023-06-25 22:55:40 -0700

    addded

--- undefined.js
+++ undefined.js
@@ -3,5 +3,7 @@
 1. variable that is not initialized will undedined
 2. function with no return
 3. parameter that is not passed will be undefined
+4. if return has nothing on the right side will return undefined
+5.
 */
 
