2c230d86174d3d985500400e785e10aba135579a
sakibhossain <sakibhossainceo@gmail.com>
2023-06-25 22:57:15 -0700

    added

--- undefined.js
+++ undefined.js
@@ -4,6 +4,7 @@
 2. function with no return
 3. parameter that is not passed will be undefined
 4. if return has nothing on the right side will return undefined
-5.
+5. property that doesn't exists on an object will give you undefined
+6.
 */
 
