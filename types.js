e0be2bff2a3f6513bebe750f5213605ad001ef65
sakibhossain <sakibhossainceo@gmail.com>
2023-06-26 22:58:39 -0700

    added

--- types.js
+++ types.js
@@ -1,6 +1,11 @@
+// JS is a dynamic dete type languege
+// All data type
+
+// primitive data type
 let a = 'Sakib'
 let b = '50'
 let c = 100
 let d = true
+// non-primitive data type
 let e = {name: 'Hossain'}
 let f = [10, 20, 30]
