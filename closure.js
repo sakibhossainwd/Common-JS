af9519a21a5b02da107406687b8b7ffec4b696a7
sakibhossain <sakibhossainceo@gmail.com>
2023-07-01 17:21:26 -0700

    added

--- closure.js
+++ closure.js
@@ -1,3 +1,8 @@
+/*
+A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function's scope from an inner function.
+*/
+
+
 function stopWatch(){
     let counter = 0;
     return function(){
