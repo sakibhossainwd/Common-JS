03883fadb496898a53f8ec5ef78f988d987aadd0
sakibhossain <sakibhossainceo@gmail.com>
2023-07-02 23:09:07 -0700

    added

--- passby.js
+++ passby.js
@@ -2,9 +2,10 @@ let num1 = 5;
 let num2 = 7;
 
 function multiply(a, b){
+    a = 10;
     const result = a * b;
     return result;
 }
 
 const output = multiply(num1, num2);
-console.logI(output);
+console.log(output);
