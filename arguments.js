5ad86b7005dc5c75a1fb7b153184d5c1e648bed2
sakibhossain <sakibhossainceo@gmail.com>
2023-07-02 23:06:03 -0700

    added

--- arguments.js
+++ arguments.js
@@ -1,8 +1,13 @@
 // array like object
 function sum(a, b, c){
+    const args = [...arguments];
+    console.log(args);
+    console.log(...arguments);
     const result = a + b + c;
     return result;
 }
 
 const total = sum(10, 20, 30, 40, 50, 60);
+console.log(sum.length)
 console.log(total);
+
