8607351a2b237d6a466def732065a0eddaa13754
sakibhossain <sakibhossainceo@gmail.com>
2023-07-02 23:23:36 -0700

    added

--- passby.js
+++ passby.js
@@ -14,3 +14,14 @@ console.log(output);
 
 
 // object and array pass by referance
+let student1 = {name: 'Jalil', partner: 'Borsha'};
+let student2 = {namae: 'Raj', pertner: 'anika'};
+
+function makeMovie(couple1, couple2){
+    // console.log(couple1, couple2);
+    couple1.name = 'Ononto';
+    couple2.partner = 'mim';
+}
+// console.log(student1, student2);
+makeMovie(student1, student2);
+// console.log(student1, student2);
