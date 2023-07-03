789a8f34ed5696255250a2d74add0b2b8c5a8015
sakibhossain <sakibhossainceo@gmail.com>
2023-07-02 23:16:39 -0700

    added

--- passby.js
+++ passby.js
@@ -1,3 +1,5 @@
+// primitive types are pass by value
+
 let num1 = 5;
 let num2 = 7;
 
@@ -9,3 +11,6 @@ function multiply(a, b){
 
 const output = multiply(num1, num2);
 console.log(output);
+
+
+// object and array pass by referance
